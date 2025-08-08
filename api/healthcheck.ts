fff08debd6a7baa41e781d152a249050581afe75
antartrc-jpg <antartrc@gmail.com>
2025-08-08 11:40:19 +0200

    Delete

--- api/healthcheck.ts
+++
@@ -1,7 +0,0 @@
-export default function handler(req: any, res: any) {
-  res.status(200).json({
-    status: 'online',
-    timestamp: new Date().toISOString(),
-    message: 'Meta-Spiegel läuft erreichbar und öffentlich.',
-  });
-}
